d0f26810843009d7d2886d5d4ce021a2586baf95
Leon Zeng <ncuzengliang@163.com>
2024-03-07 13:55:59 +0800

    修改部署报错问题

--- src/utils/getPostsWithRT.ts
+++ src/utils/getPostsWithRT.ts
@@ -2,18 +2,24 @@ import type { MarkdownInstance } from "astro";
 import type { CollectionEntry } from "astro:content";
 import { slugifyStr } from "./slugify";
 
+interface PostFrontmatter {
+  title: string;
+  readingTime: string;
+  // 在这里添加其它需要的属性
+}
+
 export const getReadingTime = async () => {
   // Get all posts using glob. This is to get the updated frontmatter
-  const globPosts = import.meta.glob("../content/blog/*.md") as Promise<
-    CollectionEntry<"blog">["data"][]
-  >;
+  const globPosts = import.meta.glob("../content/blog/*.md");
 
   // Then, set those frontmatter value in a JS Map with key value pair
   const mapFrontmatter = new Map();
   const globPostsValues = Object.values(globPosts);
   await Promise.all(
     globPostsValues.map(async globPost => {
-      const { frontmatter } = await globPost();
+      const post = await globPost();
+      // 使用类型断言
+      const frontmatter = (post as { frontmatter: PostFrontmatter }).frontmatter;
       mapFrontmatter.set(
         slugifyStr(frontmatter.title),
         frontmatter.readingTime
